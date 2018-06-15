94622eb1ab3ab9a2491a6af753a8854600a32e91
Roman Bulatov <to.rb.web@gmail.com>
2018-06-15 14:04:57 +0300

    add input component, minor fixes

---
+++ input/input.vue
@@ -0,0 +1,98 @@
+<template lang="pug">
+  include template.pug
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        revalue: '',
+        focus: false,
+        hover: false,
+        valid: true,
+      };
+    },
+
+    props: {
+      disabled: Boolean,
+      autofocus: Boolean,
+      placeholder: String,
+      pattern: String,
+      value: String,
+      type: {
+        type: String,
+        default: 'text',
+      },
+    },
+
+    methods: {
+      input(event) {
+        this.valid = event.target.validity.valid;
+        this.$emit('input', event.target.value);
+      },
+    },
+
+    computed: {
+      states() {
+        return {
+          'input--focus': this.focus,
+          'input--valid': this.valid,
+          'input--invalid': !this.valid,
+          'input--disabled': this.disabled,
+        };
+      },
+    },
+  };
+
+</script>
+
+<style lang="scss">
+  .input,
+  .input__wrapper,
+  .input__control {
+    width: 100%;
+  }
+
+  .input--disabled {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+
+  .input__wrapper {
+    display: flex;
+  }
+
+  .input__control {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-fill-color: currentColor;
+    appearance: initial;
+    background-color: transparent;
+    border: none;
+    box-shadow: none;
+    color: inherit;
+    cursor: text;
+    display: inline-flex;
+    font-family: inherit;
+    font-size: inherit;
+    margin: 0;
+    outline: none;
+    padding: 0;
+    text-decoration: none;
+    text-rendering: optimizelegibility;
+    transition: var(--animation-time, 0.3s) ease-out;
+    vertical-align: middle;
+    width: 100%;
+
+    &::placeholder {
+      -webkit-text-fill-color: currentColor;
+      color: currentColor;
+      opacity: 0.5;
+      text-align: right;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 2px #488144;
+    }
+  }
+</style>
--- test/src/App.vue
+++ test/src/App.vue
@@ -1,22 +1,97 @@
 <template lang="pug">
-  div
-    template(v-for = "type in buttonTypes")
-      br
-      v-button(:type = "type") {{ type }} button
+  .root-element
+    .examples(name="button")
+      v-button v-button
+      hr
+      v-button(types="default") v-button(types="default")
+      hr
+      v-button(types="disabled") v-button(types="disabled")
+      hr
+      v-button(:disabled="true") v-button(:disabled="true")
+      hr
+      v-button(@click="alert('click')") v-button(@click="alert('click')")
+      hr
+    .examples(name="input")
+      v-input(v-model="hidden") v-input
+      hr
+      v-input(v-model="hidden") v-input with slot "before"
+        button(slot="before") Left!
+      hr
+      v-input(v-model="hidden") v-input with slot "after"
+        button(slot="after") Right!
+      hr
+      v-input(v-model="hidden") v-input with both slots
+        span(slot="before") >>>
+        div(slot="after") <<<
+      hr
+      v-input(v-model="hidden", placeholder="Placeholder...") v-input(placeholder="Placeholder...")
+      hr
+      v-input(v-model="hidden", :disabled="true") v-input(:disabled="true")
+      hr
+      v-input(v-model="hidden", :disabled="true", placeholder="???") v-input(:disabled="true", placeholder="???")
+      hr
+      v-input(v-model="hidden", :disabled="true", value="???") v-input(:disabled="true", value="???")
+      hr
+      v-input(v-model="hidden", :autofocus="true", value="v-input(:autofocus=\"true\")")
+      hr
+      v-input(v-model="message") v-input(v-model="message")
+      span v-model output: {{ message }}
+      hr
+      v-input(v-model="hidden", type="email") v-input(type="email")
 </template>
 
 <script>
-  import button from './../../button/button.vue';
+  import button from './../../button/';
+  import input from './../../input/';
 
   export default {
     components: {
       'v-button': button,
+      'v-input': input,
     },
 
     data() {
       return {
-        buttonTypes: ['default', 'disabled'],
+        message: '',
+        hidden: '',
       };
+    },
+
+    methods: {
+      alert(string) {
+        alert(string);
+      },
     },
   };
 </script>
+
+<style>
+  body {
+    margin: 0;
+    font-size: 16px;
+  }
+
+  hr {
+    width: 100%;
+  }
+
+  .examples {
+    align-items: flex-start;
+    border-bottom: thin solid;
+    border-right: thin solid;
+    display: inline-flex;
+    flex-flow: column nowrap;
+    font-family: monospace;
+    justify-content: space-between;
+    padding: 50px;
+  }
+
+  .examples::before {
+    display: block;
+    content: attr(name);
+    font-family: sans-serif;
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-bottom: 16px;
+  }
+</style>
